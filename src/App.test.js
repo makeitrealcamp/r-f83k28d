048a574fa2d35c2f6d9a241fdc81aa2e5ef2da35
German Escobar <german.escobarc@gmail.com>
2016-11-22 07:24:15 -0500

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+it('renders the value', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.value').length).toBe(1);
+});
+
+it('renders the button', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('button#inc').length).toBe(1);
+});
+
+it('increments the value', () => {
+  const wrapper = shallow(<App />);
+  wrapper.find('button').simulate('click');
+  expect(wrapper.find('.value').text()).toBe("1");
 });
