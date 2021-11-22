d62ff8b6ab353a8ea14fef1f29b740a1038464b1
Franco <francomelgar4@gmail.com>
2021-11-22 11:04:21 -0500

    Commited

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contador: 0,
+    };
+  }
+  onClickHandler() {
+    this.setState({
+      contador: this.state.contador + 1,
+    });
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.contador}</span>
+        <button id="inc" onClick={() => this.onClickHandler()}>
+          Incrementa
+        </button>
       </div>
     );
   }
